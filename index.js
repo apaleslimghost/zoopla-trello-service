dd388898e69ee204736d22e1030f17b63e6de17d
Matt Brennan <mattyb1000@gmail.com>
2016-04-16 14:09:22 +0100

    basically working

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 var zooplaTrello = require('zoopla-trello');
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 
-exports.handler = function(event, context, callback) {
-	zooplaTrello(event.property, {
+var app = express();
+app.engine('handlebars', require('express-handlebars')({
+	helpers: {
+		stringify(thing) {
+			return JSON.stringify(thing);
+		}
+	}
+}));
+app.set('view engine', 'handlebars');
+
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+	res.render('index', {trelloAppKey: process.env.TRELLO_APP_KEY});
+});
+
+app.post('/_submit', bodyParser.urlencoded({extended: false}), (req, res, next) => {
+	zooplaTrello(req.body.property, {
 		applicationKey: process.env.TRELLO_APP_KEY,
-		userToken: process.env.TRELLO_USER_TOKEN,
-		listId: process.env.TRELLO_LIST_ID,
-	}).then(card => callback(null, card), callback);
-};
+		listId: req.body.list,
+		userToken: req.body.auth,
+	})
+	.then(card => res.json(card))
+	.catch(next);
+})
+
+app.listen(3000);
